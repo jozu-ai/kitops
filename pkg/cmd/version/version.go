daec660cdd080438972e3836c86437c773ca224c
nida <unknown>
2024-02-13 11:41:04 -0500

    removed commented out code

--- pkg/cmd/version/version.go
+++ pkg/cmd/version/version.go
@@ -31,15 +31,4 @@ or verifying that you are running the expected version of jmm.`,
 	return cmd
 }
 
-func init() {
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// pushCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// pushCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
+func init() {}
