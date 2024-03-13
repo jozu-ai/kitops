204342c9dcfcd5270307f66e089fbb3c42fae4d8
Javis V. Pérez <javisperez@gmail.com>
2024-03-13 17:07:53 -0400

    Fix the docs build

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,6 @@
 // https://vitepress.dev/guide/custom-theme
 import { h, ref } from 'vue'
-import { type Theme } from 'vitepress'
+import { type Theme, inBrowser } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import PlatformSelect from './components/PlatformSelect.vue'
 import PlatformSnippet from './components/PlatformSnippet.vue'
@@ -14,15 +14,17 @@ const isPlatformModalOpen = ref(false)
 export default {
   extends: DefaultTheme,
   Layout: () => {
-    // Google tag manager
-    const noscript = document.createElement('noscript')
-    const ifr = document.createElement('iframe')
-    ifr.src = 'https://www.googletagmanager.com/ns.html?id=GTM-TFFZXCQW';
-    ifr.height = 0;
-    ifr.width = 0;
-    ifr.style = 'display:none;visibility:hidden;';
-    noscript.appendChild(ifr)
-    document.body.insertBefore(noscript, document.body.firstElementChild)
+    if (inBrowser) {
+      // Google tag manager
+      const noscript = document.createElement('noscript')
+      const ifr = document.createElement('iframe')
+      ifr.src = 'https://www.googletagmanager.com/ns.html?id=GTM-TFFZXCQW';
+      ifr.height = 0;
+      ifr.width = 0;
+      ifr.style = 'display:none;visibility:hidden;';
+      noscript.appendChild(ifr)
+      document.body.insertBefore(noscript, document.body.firstElementChild)
+    }
 
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
