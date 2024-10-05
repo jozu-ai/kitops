63b61fbf650a8e1db0d4d927a054c2ba3a94cc44
brett-hodges <128499638+brett-hodges@users.noreply.github.com>
2024-10-05 14:47:48 +0000

    homebrew: update Homebrew Formula for main

--- build/homebrew/kitops.rb
+++ build/homebrew/kitops.rb
@@ -17,16 +17,16 @@ class Kitops < Formula
 
   on_linux do
     on_arm do
-      url "https://github.com/brett-hodges/kitops/releases/download/v0.4.18/kitops-linux-arm64.tar.gz"
-      sha256 "bcf92b2cdd0006c4c35fbf138bd36d1229252bef56190038ceef8e49d103e0c5"
+      url "https://github.com/brett-hodges/kitops/releases/download/v0.4.19/kitops-linux-arm64.tar.gz"
+      sha256 "56cebc02a0ce8c2ddad0caf668226986398dfe84b4513b600a12b4797d1a0927"
     end
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/brett-hodges/kitops/releases/download/v0.4.18/kitops-linux-x86_64.tar.gz"
-        sha256 "6a725d1f8cf70c5a14cc91bbcc9bbd6039c2f4ce7c31d2a4c541252b4829aa90"
+        url "https://github.com/brett-hodges/kitops/releases/download/v0.4.19/kitops-linux-x86_64.tar.gz"
+        sha256 "f93c6d554d810c6587a952adf91d76a8f5b1ae96d5360bd9cf551a4cc09e59c3"
       else
-        url "https://github.com/brett-hodges/kitops/releases/download/v0.4.18/kitops-linux-i386.tar.gz"
-        sha256 "e355038d809c0e9e5a237a8a7f7dcf64abd8a0577ec05583cd2ac2876e7d4373"
+        url "https://github.com/brett-hodges/kitops/releases/download/v0.4.19/kitops-linux-i386.tar.gz"
+        sha256 "c8420c08e8731148f464e22b4c849232de7116ee04c4cc5c0e3bd1613b87cf36"
       end
     end
   end
