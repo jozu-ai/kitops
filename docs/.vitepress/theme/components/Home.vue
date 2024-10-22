e6031b31a2373e67c705e912b0374907ee8483b3
Javis V. Pérez <javisperez@gmail.com>
2024-10-22 14:14:38 -0400

    Update Home.vue

--- docs/.vitepress/theme/components/Home.vue
+++ docs/.vitepress/theme/components/Home.vue
@@ -280,7 +280,7 @@ const subscribeToNewsletter = async () => {
     <li>
       <div class="text-off-white">🏃‍♂️‍ Local dev mode</div>
       <p class="p2 mb-4 text-gray-06">Kit's Dev Mode lets your run an LLM locally, configure it, and prompt/chat with it instantly</p>
-      <a class="text-off-white font-bold flex items-center gap-2 text-base" href="https://jozu.ml/organization/jozu-quickstarts">
+      <a class="text-off-white font-bold flex items-center gap-2 text-base" href="/docs/why-kitops.html">
         LEARN MORE
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
           <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z" fill="#ECECEC"/>
