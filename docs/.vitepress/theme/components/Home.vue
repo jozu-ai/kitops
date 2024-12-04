d64f61e3e691520252f2b3cd6d5817d2a36b7d0b
Javis V. Pérez <javisperez@gmail.com>
2024-12-04 15:54:14 -0400

    Add JS to handle hashtags

--- docs/.vitepress/theme/components/Home.vue
+++ docs/.vitepress/theme/components/Home.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { isClient } from '@vueuse/core'
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import { Vue3Marquee } from 'vue3-marquee'
 import Accordion from './Accordion.vue'
 import vGaTrack from '../directives/ga'
-import axios, { type AxiosError } from 'axios'
+import axios from 'axios'
 import VueTurnstile from 'vue-turnstile'
 
 const error = ref('')
@@ -91,6 +91,21 @@ const subscribeToNewsletter = async () => {
     isBusy.value = false
   }
 }
+
+onMounted(() => {
+  setTimeout(() => {
+    // check if there's an anchor link in the url and if so, scroll to that element id
+    if (location.hash) {
+      const el = document.querySelector(location.hash)
+      if (el && el instanceof HTMLElement) {
+        el.scrollIntoView({
+          behavior:'smooth',
+          block: 'center',
+        })
+      }
+    }
+  }, 100)
+})
 </script>
 
 <template>
@@ -126,7 +141,7 @@ const subscribeToNewsletter = async () => {
   </video>
 </div>
 
-<div v-if="!isSubscribed" class="mt-32 md:mt-20 xl:mt-40 pt-20 px-6 md:px-12 content-container" id="join">
+<div v-if="!isSubscribed" class="mt-32 md:mt-40 xl:mt-60 px-6 md:px-12 content-container" id="join">
   <h2 class="text-center">stay informed About Kitops</h2>
 
   <div class="text-center max-w-[600px] mx-auto mt-12">
