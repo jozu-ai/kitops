16b83a5cbd32ea4221234e0551731f0c2443255a
Angel Misevski <amisevsk@gmail.com>
2024-12-17 14:26:32 -0600

    Add library for cloning Git repositories locally
    Add a library package for cloning Git repositories. Rather than using a
    Go-based git library (such as go-git) we just defer to calling the git
    CLI directly. This is because libraries are (in my experience)
    significantly slower and consume significantly more memory.
    
    For git-lfs clones, we first clone the repository itself, with LFS
    disabled, then download the LFS files.

---
+++ pkg/lib/git/cleanup.go
@@ -0,0 +1,45 @@
+// Copyright 2025 The KitOps Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package git
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+// CleanGitMetadata removes all git-related files from a repository
+func CleanGitMetadata(dir string) error {
+	filesToClean := []string{
+		".git",
+		".gitattributes",
+		".gitignore",
+	}
+
+	for _, path := range filesToClean {
+		fullPath := filepath.Join(dir, path)
+		if err := os.RemoveAll(fullPath); err != nil {
+			if errors.Is(err, fs.ErrNotExist) {
+				continue
+			}
+			return fmt.Errorf("error removing %s: %w", path, err)
+		}
+	}
+	return nil
+}
